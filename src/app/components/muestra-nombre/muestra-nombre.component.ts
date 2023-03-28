6f3f1d2971a3100254ec6d28baf640cab899325d
Yaqi <yaqiye22@gmail.com_>
2023-03-28 02:28:21 +0200

    Sección 20 terminada. 27_03_2023

---
+++ src/app/components/muestra-nombre/muestra-nombre.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-muestra-nombre',
+  templateUrl: './muestra-nombre.component.html'
+})
+export class MuestraNombreComponent implements OnInit, OnChanges {
+
+  @Input() nombre!: string;
+  
+  constructor() {
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log( changes );
+  }
+
+  ngOnInit(): void {
+    
+  }
+}
---
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -0,0 +1,60 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html'
+})
+export class Pagina1Component 
+       implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, 
+                  AfterViewInit, AfterViewChecked, OnDestroy {
+  
+  nombre              : string = 'Fernando';
+  segundos            : number = 0;
+  timerSubscription!  : Subscription;
+
+  constructor () {
+    console.log( 'constructor' );
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+    this.timerSubscription.unsubscribe();
+    console.log('Timer limpiado');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.timerSubscription = interval(1000).subscribe( i => {
+      this.segundos = i;
+    })
+  }
+  
+  guardar() {
+
+  }
+}
